c5e2299a8d6191fbb9001e4d92dfbd8521462dbe
Alison-Lee-github <74337495+Alison-Lee-github@users.noreply.github.com>
2023-03-20 20:53:37 -0400

    Add files via upload

--- InstructionFetch.sv
+++ InstructionFetch.sv
@@ -1,21 +1,27 @@
 
-module InstructionFetch(reset,clk,PC_jump,PC_adderOut,PC_source,PC_enable,instruction1,instruction2);
+module InstructionFetch(reset,clk,PC_source,PC_enable,PC_jump,fetch_output1,fetch_output2,PC_adderOut,IF_flush);
 
-parameter bitsize= 32;
+parameter bitsize = 11;
 
-input clk,reset,PC_enable,PC_source;
+input clk,reset,IF_flush;
+input PC_enable,PC_source;
+input [bitsize-1:0] PC_jump;
+logic [bitsize-1:0] PC_out; 
+logic [bitsize-1:0] PC_next; 
 
-input [bitsize-1:0] PC_jump; 
-output [31:0] instruction1, instruction2;
+logic  [31:0] instruction1, instruction2;
 output [bitsize-1:0] PC_adderOut;
+output [31:0] fetch_output1,fetch_output2; //will flush insturction or keep 
 
 
-
-
-program_counter pc(PC_next,PC_out,clk,reset,PCenable);
-PCAdder(PC_out,PC_adderOut);
-mux2_to_1_32BIT Jump_or_Pc4(PC_next,PC_adderOut,PC_jump,PC_source);
+ProgramCounter pc(PC_next,PC_out,reset,clk,PCenable);
+PCAdder pcadder(PC_out,PC_adderOut);
+mux2_to_1_11BIT Jump_or_Pc4(PC_next,PC_adderOut,PC_jump,PC_source);
 instruction_memory insMem(PC_out,instruction1,instruction2);
+//adds two modules
+mux2_to_1_32BIT instruction_flush_one(fetch_output1,instruction1,0,IF_flush); 	 
+mux2_to_1_32BIT instruction_flush_two(fetch_output2,instruction2,0,IF_flush); 	
+
 
 endmodule
 
