2372460b825ae831630de62252705a1f86f85951
Alison-Lee-github <74337495+Alison-Lee-github@users.noreply.github.com>
2023-04-15 18:38:22 -0400

    Add files via upload

--- InstructionFetch.sv
+++ InstructionFetch.sv
@@ -1,65 +1,45 @@
-
-/*
-module InstructionFetch(reset,clk,PC_source,PC_enable,PC_branch,IF_flush,
-			fetch_output1,fetch_output2,PC_adderOut);
-
-parameter bitsize = 11;
-
-input clk,reset,IF_flush;
-input PC_enable,PC_source;
-input [bitsize-1:0] PC_branch;
-logic [bitsize-1:0] PC_out; 
-logic [bitsize-1:0] PC_next; 
-
-logic  [31:0] instruction1, instruction2;
-output [bitsize-1:0] PC_adderOut;
-output [31:0] fetch_output1,fetch_output2; //will flush insturction or keep 
+module InstructionFetch(clk, reset, enablePC, instruction1, instruction2,branch_PC,branch_flag);
 
 
-ProgramCounter pc(PC_next,PC_out,reset,clk,PCenable);
-PCAdder pcadder(PC_out,PC_adderOut);
-mux2_to_1_11BIT Branch_or_Pc4(PC_next,PC_adderOut,PC_branch,PC_source);
-instruction_memory insMem(PC_out,instruction1,instruction2);
-//adds two modules
-mux2_to_1_32BIT instruction_flush_one(fetch_output1,instruction1,0,IF_flush); 	 
-mux2_to_1_32BIT instruction_flush_two(fetch_output2,instruction2,0,IF_flush); 	
+input clk,reset,enablePC,branch_flag;
+input [31:0] branch_PC;
+
+output logic  [31:0] instruction1, instruction2;  
 
 
-endmodule
-*/
+reg [31:0] mem[0:249]; 
+logic [31:0] address; 
+logic [31:0] PC; 
+assign address=PC; 
 
-module InstructionFetch(reset,clk,PC_source,PC_enable,PC_branch,IF_flush,
-			fetch_output1,fetch_output2,PC_adderOut);
-
-parameter bitsize = 11;
-
-input clk,reset,IF_flush;
-input PC_enable,PC_source;
-
-input [bitsize-1:0] PC_branch;
-logic [bitsize-1:0] PC_out; 
-logic [bitsize-1:0] PC_next; 
-
-logic  [31:0] instruction1, instruction2;
-output logic [bitsize-1:0] PC_adderOut;
-output [31:0] fetch_output1,fetch_output2; //will flush insturction or keep 
-
-reg [7:0][7:0] mem[0:249]; 
-logic [bitsize-1:0] address; 
-assign address=PC_out; 
-initial begin
- $readmemh("code.txt",mem);
+initial begin 
+	$readmemh("code.txt",mem); // Read the file of the instructions; 
 end 
-always_comb begin
+
+always_ff @(posedge clk) begin
 	
-        if(PC_enable==1) begin 
-	  	PC_adderOut = PC_adderOut+8; 
+	if (enablePC == 1) begin 
+		if(branch_flag==0) begin 
+	  	PC <= PC+8;  
+	end 
+	else begin 
+	PC<=branch_PC; 	
+		
+	end 
+		
+	
         end 
+	
+	else begin
+		PC <= PC;
+	end
+end
  
-if(IF_flush==0) begin //instruction_memory 
-	instruction1 = mem[address>>3][7:4];
-	instruction2 = mem[address>>3][3:0]; 
-end 
+always_comb begin
+	       
+	instruction1 = mem[(address>>2)-1];    //instruction_memory 
+	instruction2 = mem[(address>>2)];  //instruction_memory 
+	
 end 
 
 
