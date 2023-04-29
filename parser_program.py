cee29c06d66e27df0d8090611ee0ea2bf1d4513f
Alison-Lee-github <74337495+Alison-Lee-github@users.noreply.github.com>
2023-04-29 18:39:57 -0400

    Add files via upload
    Edit ordering of opcodes

--- parser_program.py
+++ parser_program.py
@@ -41,6 +41,7 @@ def get_op_length(opcode):
 code=open('code.txt', 'r') 
 strip_line=""
 binary_code_array=[]
+arg_array=[]
 new_arg=""
 new_binary=""
 def get_format_size(opcode_length,binary_number,current_arg):
@@ -74,13 +75,33 @@ def get_format_size(opcode_length,binary_number,current_arg):
         padded_binary=binary_number.zfill(R_I18[current_arg])
     print(padded_binary)
     return padded_binary  
-        
+def order_code(opcode_length,array):
+    final_string=""
+    if(opcode_length==11):
+        final_string=array[2]+array[1]+array[0]
+        #padded_binary=binary_number.zfill(RR_or_RI7[current_arg])
+        #print("current_arg_size is" +str(RR_or_RI7[current_arg]))
+    elif(opcode_length==4):
+        final_string=array[0]+array[2]+array[1]+array[3]
+       # padded_binary=binary_number.zfill(RRR[current_arg])
+    elif(opcode_length==8):
+        final_string=array[3]+array[1]+array[0]
+       # padded_binary=binary_number.zfill(R_I10[current_arg])
+    elif(opcode_length==9):
+        final_string=array[1]+array[0]
+       # padded_binary=binary_number.zfill(R_I16[current_arg])
+    elif(opcode_length==7):
+        final_string=array[1]+array[0]
+      #  padded_binary=binary_number.zfill(R_I18[current_arg])
+    return final_string 
+    
 while True:
     file_line=code.readline()
     if len(file_line) == 0: #break when there are no more strings 
          break
-    split_line=file_line.split() # separate keyword from registers  
-    print(split_line)
+    split_line=file_line.split() # separate keyword from registers
+    print("The length of the file is")
+    print(len(file_line))
     #print("Opcode is "+split_line[0])
     new_binary=rewrite_binary(split_line[0]) #opcode
     #print("New binary is "+new_binary)
@@ -94,16 +115,20 @@ while True:
         print("i is "+str(i))
         #print(bin(int(arg)))
         binary_arg=bin(int(arg))[2:]
+        print("Current binary arg is "+binary_arg)
         binary_arg=get_format_size(opcode_size,binary_arg,i)
+        arg_array.append(binary_arg)
         #print(binary_arg)
-        new_arg=new_arg+binary_arg
-    print(new_arg)
-    new_line=new_binary+new_arg
+        #new_arg=new_arg+binary_arg
+    #print("The new  arg is")
+   # print(binary_arg)
+    new_line=order_code(opcode_size,arg_array)
+    new_line=new_binary+new_line
     print("the new line character length is "+str(len(new_line)))
     #print(new_line)
     new_arg="" #clear new_arg `
     binary_code_array.append(new_line) #add line to array 
-    
+    arr_array=[]
 code.close()
 with open('binary_code.txt','w') as binary_code:
     for binary in binary_code_array:
